757f49bc0bed8d7240ed5fd999c98efb1c1011ec
Matt Ellison <mattbryanellison@gmail.com>
2021-01-15 16:19:15 -0500

    [continues]cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,6 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {getCartThunk} from '../store/cart'
+import {
+  Grid,
+  Paper,
+  makeStyles,
+  CardHeader,
+  Typography,
+  Divider,
+} from '@material-ui/core'
 
 export class Cart extends React.Component {
   constructor(props) {
@@ -11,10 +19,50 @@ export class Cart extends React.Component {
   componentDidMount() {}
 
   render() {
+    const classes = {
+      cart: {
+        height: '100vh',
+      },
+      paper: {
+        height: '100vh',
+      },
+      details: {
+        paddingLeft: '24px',
+      },
+      innerGrid: {
+        paddingTop: '20px',
+      },
+    }
     console.log('this is props', this.props.cart)
     return (
-      <div>
-        <h3>THIS WILL BE THE CART</h3>
+      <div style={classes.cart}>
+        <Grid style={classes.cart} container spacing={2}>
+          <Grid item xs={12} sm={9}>
+            <Paper style={classes.paper}>
+              <CardHeader title="Shopping Cart"></CardHeader>
+              <Divider></Divider>
+              <Grid container style={classes.innerGrid} spacing={2}>
+                <Grid item xs={6}>
+                  <Typography style={classes.details} variant="h7">
+                    Product Details
+                  </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography variant="h7">Quantity</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography variant="h7">Price</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography variant="h7">Total</Typography>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Paper style={classes.paper}>Left side</Paper>
+          </Grid>
+        </Grid>
       </div>
     )
   }
